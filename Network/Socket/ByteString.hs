e3f86ddd5bdde3f39912bb6336f54e0184b76032
Kazu Yamamoto <kazu@iij.ad.jp>
2019-12-03 10:40:08 +0900

    implementing Auxiliary.

--- Network/Socket/ByteString.hs
+++ Network/Socket/ByteString.hs
@@ -40,6 +40,23 @@ module Network.Socket.ByteString
     , recvMsg
     , MsgFlag(..)
     , Cmsg(..)
+    -- ** Auxiliary data
+    , Auxiliary(..)
+    , AuxiliaryID
+    , auxiliaryIPv4TTL
+    , auxiliaryIPv6HopLimit
+    , auxiliaryIPv4TOS
+    , auxiliaryIPv6TClass
+    , auxiliaryIPv4PktInfo
+    , auxiliaryIPv6PktInfo
+    , lookupAuxiliary
+    -- ** Types
+    , IPv4TTL(..)
+    , IPv6HopLimit(..)
+    , IPv4TOS(..)
+    , IPv6TClass(..)
+    , IPv4PktInfo(..)
+    , IPv6PktInfo(..)
     ) where
 
 import Data.ByteString (ByteString)
@@ -47,6 +64,7 @@ import Data.ByteString (ByteString)
 import Network.Socket.ByteString.IO hiding (sendTo, sendAllTo, recvFrom)
 import qualified Network.Socket.ByteString.IO as G
 import Network.Socket.Types
+import Network.Socket.ByteString.Auxiliary
 
 -- ----------------------------------------------------------------------------
 -- ** Vectored I/O
